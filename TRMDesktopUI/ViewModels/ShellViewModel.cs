61132a71b1e1ce77772950355b8499ed62a1f722
Arthuthu <arthurgeromello@hotmail.com>
2022-07-11 15:36:44 -0300

    End of WPF Login Form Creation
    #11 on playlist.

--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Caliburn.Micro;
 using System.Threading.Tasks;
 
 namespace TRMDesktopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel _loginVM;
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _loginVM = loginVM;
+            ActivateItem(_loginVM);
+        }
     }
 }
