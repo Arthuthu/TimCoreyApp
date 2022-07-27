984c1ddff8ae102209818eaf831663e0ec58e55a
Arthuthu <arthurgeromello@hotmail.com>
2022-07-27 15:48:09 -0300

    Added proper connection string

--- TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -13,9 +14,14 @@ namespace TRMDataManager.Library.Internal.DataAccess
     internal class SqlDataAccess : IDisposable
     {
 
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public string GetConnectionString(string name)
         {
-            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+            return _config.GetConnectionString(name);
         }
 
         public List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
@@ -74,6 +80,9 @@ namespace TRMDataManager.Library.Internal.DataAccess
         }
 
         private bool isClosed = false;
+        private IConfiguration _config;
+        private readonly IConfiguration config;
+
         public void CommitTransaction()
         {
             _transaction?.Commit();
