7e2d869d858e537dc57913db2d854bd4b2e3ccf9
Arthuthu <arthurgeromello@hotmail.com>
2022-07-27 16:58:02 -0300

    Added JWT Authentication

--- TRMApi/Controllers/InventoryController.cs
+++ TRMApi/Controllers/InventoryController.cs
@@ -20,6 +20,7 @@ namespace TRMApi.Controllers
             _config = config;
         }
         [Authorize(Roles = "Manager,Admin")]
+        [HttpGet]
         public List<InventoryModel> Get()
         {
             InventoryData data = new InventoryData(_config);
@@ -28,6 +29,7 @@ namespace TRMApi.Controllers
         }
 
         [Authorize(Roles = "Admin")]
+        [HttpPost]
         public void Post(InventoryModel item)
         {
             InventoryData data = new InventoryData(_config);
