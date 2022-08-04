5b6b9e2fb4826d7e888bfb5bc4035e8bd1227041
Arthuthu <arthurgeromello@hotmail.com>
2022-08-04 14:32:24 -0300

    Removed site

--- MySite/SQLDatabase/Alunos.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Alunos]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Email] NVARCHAR(100) NOT NULL, 
-    [FirstName] NVARCHAR(100) NOT NULL, 
-    [LastName] NVARCHAR(100) NOT NULL, 
-    [PhoneNumber] NVARCHAR(100) NOT NULL
-)
